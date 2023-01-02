804d0dd5ea7d7e62db39a7a9251192d5e8ccf744
rymarktron <87295558+rymarktron@users.noreply.github.com>
2023-01-02 02:29:14 -0500

    remove scout thing (need to fix)

--- pages/experiences/index.js
+++ pages/experiences/index.js
@@ -19,12 +19,12 @@ const Experiences = () => {
             <i>May - August 2022</i>
             <br/>
             <p>
-            During Ryan’s co-op term at the Institute for Quantum Computing (IQC), he had the opportunity to work on designing the engineering 
+            During Ryan&#39;s co-op term at the Institute for Quantum Computing (IQC), he had the opportunity to work on designing the engineering 
             system for cutting-edge research projects in the field of quantum information science. He was part of a team of researchers working
             on the development of novel quantum algorithms and technologies, and he was able to contribute my engineering experience to several 
             key projects throughout my time at IQC. During his term, one of the major projects that he worked on was the design of an optical 
             beam delivery system. This sub-system was responsible for sending a laser beam to a trapped ion, and it used cameras and other sensors 
-            to calibrate the ion`&apos`s position and correct it as needed.
+            to calibrate the ion&#39;s position and correct it as needed.
 
 
             </p>
@@ -33,7 +33,7 @@ const Experiences = () => {
 
             <Image src = "/logos/Amphenol-logo.svg" width = {200} height = {80}/><br/>
             <i>September - December 2021</i>
-            <p>During Ryan`&apos`s co-op term as a Process Engineer at an electromechanical manufacturing company, they gained valuable hands-on experience 
+            <p>During Ryan&#39;s co-op term as a Process Engineer at an electromechanical manufacturing company, they gained valuable hands-on experience 
             in the production of wire harnesses. One of their main responsibilities was to create engineering drawings for the wire harnesses on AutoCAD, ensuring 
             that all necessary dimensions and specifications were accurately captured.
 
@@ -56,8 +56,14 @@ const Experiences = () => {
             
             <h1>Design Team Experiences</h1>
             <h2>Mars Rover Design Team</h2>
-            <p>I have been involved in the Mars Rover Robotics design team. I started in the mechanical team and learned manufacturing design and 
-            how to machine parts using the student design ttmachine shop. </p>
+            <p>As a member of the mechanical subteam of the Mars Rover Design Team at the University of Waterloo, Ryan has been able to apply his skills 
+            and knowledge in mechanical engineering to help design and build a prototype Mars rover. He has worked closely with the rest of the team to 
+            design and fabricate various components of the rover, and has contributed to the development of the rover&#39;s overall mechanical system.
+
+            One of the most rewarding aspects of Ryan&#39;s involvement with the Mars Rover Design Team has been the opportunity to work on a real-world 
+            project that has the potential to have a significant impact. He has enjoyed the challenge of finding creative solutions to complex problems, 
+            and has learned a lot about teamwork and project management through his participation in the team.
+            </p>
 
             <h1>Other Experiences</h1>
             Some of experiences worth mentioning is my involvement in Scouts at a local and national level.
